81302d06f048e5f573e6d2b0c50e79707fa5e546
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-16 14:01:32 -0300

    Corrige erros de identação

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -10,12 +10,14 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html",
-    language=LanguageModel.list_dicts(),
-    text_to_translate="O que deseja traduzir",
-    translate_from="pt",
-    translate_to="en",
-    translated="Tradução")
+    return render_template(
+        "index.html",
+        language=LanguageModel.list_dicts(),
+        text_to_translate="O que deseja traduzir",
+        translate_from="pt",
+        translate_to="en",
+        translated="Tradução"
+    )
 
 
 # Req. 6
