191bc0b1e14c5b1a2f443e5e1858923cc069f910
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-16 15:42:17 -0300

    Corrige erros de identação

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -5,7 +5,7 @@ from src.models.history_model import HistoryModel
 # Req. 7
 def test_request_history():
     listing = json.loads(HistoryModel.list_as_json())
-    
+
     assert listing[0]["text_to_translate"] == "Hello, I like videogame"
     assert listing[0]["translate_from"] == "en"
     assert listing[0]["translate_to"] == "pt"
@@ -13,4 +13,3 @@ def test_request_history():
     assert listing[1]["text_to_translate"] == "Do you love music?"
     assert listing[1]["translate_from"] == "en"
     assert listing[1]["translate_to"] == "pt"
-
